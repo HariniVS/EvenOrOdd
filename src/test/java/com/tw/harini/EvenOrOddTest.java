9bca0b35f81728a827448df8264c9088e9b585f5
Harini V S <harinivs@thoughtworks.com>
2017-06-06 15:47:46 +0530

    add a failing test after linking to travis ci

--- src/test/java/com/tw/harini/EvenOrOddTest.java
+++ src/test/java/com/tw/harini/EvenOrOddTest.java
@@ -10,15 +10,20 @@ import static org.junit.Assert.assertEquals;
 public class EvenOrOddTest {
 
     @Test
-    public  void shouldCheckOddOrEven(){
+    public void shouldCheckOddOrEven() {
         EvenOrOdd evenOrOdd = new EvenOrOdd();
         assertEquals("even", evenOrOdd.checkEvenOrOdd(2));
     }
 
     @Test
-    public  void shouldReturnEven(){
+    public void shouldReturnEven() {
         EvenOrOdd evenOrOdd = new EvenOrOdd();
         assertEquals("even", evenOrOdd.checkEvenOrOdd(4));
     }
 
+    @Test
+    public void shouldReturnOdd() {
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+        assertEquals("odd", evenOrOdd.checkEvenOrOdd(4));
+    }
 }
